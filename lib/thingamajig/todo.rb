0eebd8ac1f4bef6951ce08daaaa5bf7b8c5dfb40
David Verhasselt <david@crowdway.com>
2022-02-19 18:49:06 +0200

    Add utils to Todo

--- lib/thingamajig/todo.rb
+++ lib/thingamajig/todo.rb
@@ -20,12 +20,47 @@ class Thingamajig
       activation_date && activation_date <= Date.today.to_time
     end
 
+    # Use `activate!` to move to Today with today's date as the activation
+    # date. This moving to Today list means it will also pop up at the top
+    # of the Today list in the UI.
+    #
+    # This is different from using the `schedule` command which retains the
+    # position in Today list from earlier.
+    def activate!
+      osa_object.move(to: Thingamajig::List.today.osa_object)
+    end
+
+    def deactivate!
+      osa_object.move(to: Thingamajig::List.anytime.osa_object)
+    end
+
     def activation_date=(new_activation_date)
       if completed? || canceled?
         self.completion_date = nil
       end
 
-      osa_object.schedule(for: new_activation_date)
+      if new_activation_date.present?
+        osa_object.schedule(for: new_activation_date)
+      else
+        deactivate!
+      end
+    end
+
+    # Adds a todo to the bottom of the project
+    def project=(new_project)
+      if new_project
+        super(new_project.osa_object)
+      else
+        osa_object.project.delete
+      end
+    end
+
+    def area=(new_area)
+      if new_area
+        super(new_area.osa_object)
+      else
+        osa_object.area.delete
+      end
     end
 
     def inspect
